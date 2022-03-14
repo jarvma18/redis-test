8b6069aa19586e67a4340c8408112511f816a7d5
jarvma18 <markusmjarvinen@outlook.com>
2022-03-14 23:41:26 +0200

    Modified tester.

--- tester/index.js
+++ tester/index.js
@@ -4,23 +4,26 @@ const axios = require('axios');
 const url = 'http://api:8080/test/';
 
 setInterval(function() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
+    console.time(i);
     axios.get(url + i)
     .then(function (response) {
+      console.timeEnd(i);
       // handle success
-      console.log(i, response.data);
+      console.log(i, response.data.value, response.data.timestamp);
     })
     .catch(function (error) {
       // handle error
       console.log(error);
     });
   }
+  console.log('###################################');
 }, 1000);
 
 setInterval(function() {
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < 5; i++) {
     axios.post(url + i, {
-      value: Math.random(),
+      value: Math.floor(Math.random() * 101),
       errorMessage: null,
       timestamp: new Date()
     })
